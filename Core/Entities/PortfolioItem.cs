ce748e86ec7558cfe54377205ac520801c34b5b3
yahya serhan <yahyaserhanmoon@gmail.com>
2025-08-04 01:54:53 +0300

    Add project files.

---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectNamke { get; set; }
+        public string  Description { get; set; }
+        public string  ImageUrl { get; set; }
+    }
+
+
+
+}
